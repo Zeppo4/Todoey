5cbfc4a888e747d6b69b73db6514e4eeed9c2b0b
Zeppoware <MacUser@macusermacbook9>
2019-09-16 19:55:02 -0500

    added check mark to app

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mac User on 9/16/19.
+//  Copyright © 2019 ZeppoWare. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
